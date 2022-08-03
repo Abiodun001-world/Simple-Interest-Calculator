ba7456ef9db50bfcd4f4745a87f97b679dbd164b
Abiodun001-world <adekunleabiodun354@gmail.com>
2022-08-03 20:45:01 +0100

    The code is working perfectly

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function showMessage() {
+	document.getElementById("result").style.display = "block";
+};
+
+function compute() {
+	var principal = document.getElementById("principal").value;
+	if(parseInt(principal) < 1) {
+		alert("Enter a positive number");
+		document.getElementById("principal").focus();
+		return;
+	}
+
+
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+
+	var interest = principal * years * rate / 100;
+
+	var today = new Date();
+	var yearOfWithdrawal = parseInt(today.getFullYear())+parseInt(years);
+
+	document.getElementById("renderoutput").innerHTML = ""+principal;
+	document.getElementById("interestoutput").innerHTML = ""+rate;
+	document.getElementById("amountoutput").innerHTML = ""+interest;
+	document.getElementById("yearoutput").innerHTML = ""+yearOfWithdrawal;
+	showMessage();
+
+	return false;
+};
+
+function showRange() {
+	var rate = document.getElementById("rate").value;
+	document.getElementById("ratevalue").innerHTML = rate;
 }
+
+
+
+
+
+
+
+
+
+
+
+
         
