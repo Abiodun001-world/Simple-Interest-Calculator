e292f4277ee56e56cc26ca4d5d9668b59e746058
Abiodun001-wold <adekunleabiodun354@gmail.com>
2023-08-28 04:54:56 +0100

    added  es6 class to the script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //};
 // making the compute interest responsive
 function compute() {
-	var principal = document.getElementById("principal").value;
+	const principal = document.getElementById("principal").value;
 	if(parseInt(principal) < 1) {
 		alert("Enter a positive number");
 		document.getElementById("principal").focus();
@@ -11,13 +11,13 @@ function compute() {
 	}
 
      // assigning rate, years and ptincipal to a value
-	var rate = document.getElementById("rate").value;
-	var years = document.getElementById("years").value;
+	const rate = document.getElementById("rate").value;
+	const years = document.getElementById("years").value;
 
-	var interest = principal * years * rate / 100;
+	const interest = principal * years * rate / 100;
 
-	var today = new Date();
-	var yearOfWithdrawal = parseInt(today.getFullYear())+parseInt(years);
+	const today = new Date();
+	const yearOfWithdrawal = parseInt(today.getFullYear())+parseInt(years);
 // showing the message of principal,rate,interest and year of withdrawal
 	document.getElementById("renderoutput").innerHTML = ""+principal;
 	document.getElementById("interestoutput").innerHTML = ""+rate;
@@ -29,7 +29,7 @@ function compute() {
 };
 
 function showRange() {
-	var rate = document.getElementById("rate").value;
+	const rate = document.getElementById("rate").value;
 	document.getElementById("ratevalue").innerHTML = rate;
 }
 
